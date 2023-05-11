8c61fb2259c016c8de8bfbec63a9aa275ab855fd
Dhruv-232 <118102311+Dhruv-232@users.noreply.github.com>
2023-05-11 22:53:56 +1000

    App.js finished

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,25 @@
+import React from 'react';
+import { useState } from 'react';
 import './App.css';
-import "bootstrap/dist/css/bootstrap.min.css";
+import Header from './components/Header'
+import AboutMe from './components/AboutMe'
+import Portfolio from './components/Portfolio'
+import ContactMe from './components/ContactMe'
+import Resume from './components/Resume'
+import Footer from './components/Footer'
+
 
 function App() {
-  return (
-    <h2>Portfolio website working???</h2>
-  );
+  const [currentPage, setCurrentPage] = useState('AboutMe') 
+  const changePage = (page)=>{setCurrentPage(page)}
+  return <>
+  <Header currentPage={currentPage} changePage={changePage}/>
+  {currentPage === 'AboutMe' ? <AboutMe /> :' '}
+  {currentPage === 'Portfolio' ? <Portfolio /> : ' '}
+  {currentPage === 'ContactMe' ? <ContactMe /> : ' '}
+  {currentPage === 'Resume' ? <Resume /> : ' '}
+  <Footer />
+  </>
 }
 
 export default App;
