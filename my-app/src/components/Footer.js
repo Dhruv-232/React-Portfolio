161337136dcf24cec9bc526ef8bdf4ffcb529d3e
Dhruv-232 <118102311+Dhruv-232@users.noreply.github.com>
2023-05-11 22:48:45 +1000

    Added the footer

---
+++ my-app/src/components/Footer.js
@@ -0,0 +1,12 @@
+function Footer() {
+    return (
+        <>
+            <div className='footer'>
+                <a className="footer-links" href="https://github.com/zafarfast">GitHub</a>
+                <a className="footer-links" href="https://www.linkedin.com/in/zafarahmed/">Linkedin</a>
+                <a className="footer-links" href="https://twitter.com/MdZafarAhmed3/">Twitter</a>
+            </div>
+        </>)
+}
+
+export default Footer;
